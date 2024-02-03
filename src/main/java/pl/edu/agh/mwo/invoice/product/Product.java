fdce15ee2055da88775ca01d1863f2eb110eb805
rysz <konrad.michal.rysz@gmail.com>
2024-02-03 15:07:35 +0100

    Implemented all tests for product

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,8 +10,25 @@ public abstract class Product {
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
+        if (name == null || name.equals("")) {
+            throw new IllegalArgumentException("Product name cannot be null or empty.");
+        }
+        //ważna kolejność, na odwrót nie zadzaiała
+//            throw new NameIsNullException;
+//        if (name == ""){
+//                throw new IllegalArgumentException("Product name cannot be empty");
+//        }
         this.name = name;
+
+        if (price == null || price.compareTo(BigDecimal.ZERO)<0) {
+            throw new IllegalArgumentException("Price cannot be null");
+        }
+
+//        if (price.compareTo(BigDecimal.ZERO)<0) {
+//            throw new IllegalArgumentException("Product price cannot be negative");
+//        }
         this.price = price;
+
         this.taxPercent = tax;
     }
 
