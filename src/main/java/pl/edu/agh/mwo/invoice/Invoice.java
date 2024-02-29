3e0a89d083e0a9fe17f8e5117fefecbc07724033
Konrad Rysz <konrad.michal.rysz@gmail.com>
2024-02-29 21:58:43 +0100

    Adding the getSubtotal method

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,6 +1,7 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import pl.edu.agh.mwo.invoice.product.Product;
@@ -9,15 +10,34 @@ public class Invoice {
     private Collection<Product> products;
 
     public void addProduct(Product product) {
-        // TODO: implement
+        addProduct(product, 1);
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+        if (product != null) {
+            if (quantity > 0) {
+                if (products == null) {
+                    products = new ArrayList<>();
+                }
+                for (int i = 0; i < quantity; i++) {
+                    products.add(product);
+                }
+            }
+            else {
+                throw new IllegalArgumentException("Quantity must be positive");
+            }
+        }
+        else {
+            throw new IllegalArgumentException("Product cannot be null");
+        }
     }
 
     public BigDecimal getSubtotal() {
-        return null;
+        BigDecimal subtotal = BigDecimal.ZERO;
+        for (Product product : products) {
+            subtotal = subtotal.add(product.getPrice());
+        }
+        return subtotal;
     }
 
     public BigDecimal getTax() {
